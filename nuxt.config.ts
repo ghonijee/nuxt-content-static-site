a327a8afbceacbab095df6a31b840a6f2267a1a1
ghonijee <akughoni@gmail.com>
2023-01-14 23:06:58 +0700

    add github action for push and PR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,11 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-16',
       viewport: 'width=500, initial-scale=1',
-      title: "Ahmad Yunus Blog|Portofolio"
+      title: 'Ahmad Yunus Blog|Portofolio',
+      // favicon: '~/public/favicon.svg',
       bodyAttrs: {
         class: 'bg-slate-900 text-gray-100'
-      },
-      
+      }
     }
   },
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/robots', 'nuxt-simple-sitemap']
